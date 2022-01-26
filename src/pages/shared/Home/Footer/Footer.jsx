f637de760d06367a580baae1a03b11a2694f01dd
coder71 <kamrunnaher877@gmail.com>
2022-01-27 02:17:49 +0600

    footer section completed

---
+++ src/pages/shared/Home/Footer/Footer.jsx
@@ -0,0 +1,135 @@
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelopeOpen,
+  faMap,
+  faPhoneSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import sajek from '../../../../assets/sajek.jpg';
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-wrap justify-around bg-black text-white py-6 space-y-6 space-x-6">
+      {/* logo and contacts */}
+      <div className="pl-6 space-y-6">
+        <Link to="/" className="text-6xl font-semibold text-blue-400 pt-2">
+          Trellas
+        </Link>
+        {/* contacts */}
+        <div className="space-y-4">
+          <div className="flex items-center space-x-4">
+            <FontAwesomeIcon
+              className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+              icon={faEnvelopeOpen}
+              style={{ fontSize: 24 }}
+            />
+            <p>trellas@trellas.com</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <FontAwesomeIcon
+              icon={faPhoneSquare}
+              className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+              style={{ fontSize: 24 }}
+            />
+            <p>+88019xxxxxx</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <FontAwesomeIcon
+              icon={faMap}
+              className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+              style={{ fontSize: 24 }}
+            />
+            <p>Findus, Mars</p>
+          </div>
+        </div>
+        {/* social */}
+        <div className="space-x-4">
+          <FontAwesomeIcon
+            icon={faFacebook}
+            className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+            style={{ fontSize: 24 }}
+          />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+            style={{ fontSize: 24 }}
+          />
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+            style={{ fontSize: 24 }}
+          />
+          <FontAwesomeIcon
+            icon={faYoutube}
+            className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500"
+            style={{ fontSize: 24 }}
+          />
+        </div>
+      </div>
+
+      {/* posts */}
+      <div>
+        <h3 className="text-2xl border-l-4 border-l-rose-600 pl-3 mb-6">
+          Recent posts
+        </h3>
+        {/* recent post blog links */}
+        <div className="space-y-6">
+          <div className="flex items-center">
+            <img
+              src={sajek}
+              alt="sajek"
+              className="w-20 transform hover:scale-150 transition-transform duration-700 cursor-pointer"
+            />
+            <div className="pl-3">
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                blog title
+              </p>
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                january 27 2022
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <img
+              src={sajek}
+              alt="sajek"
+              className="w-20 transform hover:scale-150 transition-transform duration-700 cursor-pointer"
+            />
+            <div className="pl-3">
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                blog title
+              </p>
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                january 27 2022
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <img
+              src={sajek}
+              alt="sajek"
+              className="w-20 transform hover:scale-150 transition-transform duration-700 cursor-pointer"
+            />
+            <div className="pl-3">
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                blog title
+              </p>
+              <p className="cursor-pointer hover:text-rose-500 transition-all ease-in duration-500">
+                january 27 2022
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/shared/Home/Home.jsx
+++ src/pages/shared/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Footer from './Footer/Footer';
 
 const Home = () => {
   return (
     <section>
       <Banner />
+      <Footer />
     </section>
   );
 };
