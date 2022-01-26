73944b8ed246463f319fe52df9894ec90c9fc99d
coder71 <kamrunnaher877@gmail.com>
2022-01-27 00:02:46 +0600

    banner modified

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import mountain from '../assets/mountain.jpg';
+import travel_one from '../assets/travel_one.jpg';
+import travel_three from '../assets/travel_three.jpg';
+import travel_two from '../assets/travel_two.jpg';
 
 const ShowCarousel = () => {
   const images = [
     {
-      src: mountain,
-      legend: 'mountain 1',
+      src: travel_one,
+      alt: 'travel 1',
     },
     {
-      src: mountain,
-      legend: 'mountain 2',
+      src: travel_two,
+      alt: 'travel 2',
     },
     {
-      src: mountain,
-      legend: 'mountain 3',
+      src: travel_three,
+      alt: 'travel 3',
     },
   ];
 
   return (
-    <Carousel autoPlay={true} showIndicators={false}>
+    <Carousel
+      autoPlay={true}
+      showIndicators={false}
+      stopOnHover={false}
+      swipeable={false}
+    >
       {images.map((image) => (
-        <div key={image.legend} style={{ height: 'calc(100vh - 100px)' }}>
+        <div key={image.legend}>
           <div>
-            <img src={image.src} alt={image.legend} className="w-full" />
+            <img
+              src={image.src}
+              alt={image.legend}
+              className="w-full"
+              style={{ height: 'calc(100vh - 90px)' }}
+            />
           </div>
-          <p className="legend ">{image.legend}</p>
         </div>
       ))}
     </Carousel>
--- src/pages/shared/Home/Banner/Banner.jsx
+++ src/pages/shared/Home/Banner/Banner.jsx
@@ -3,8 +3,24 @@ import Carousel from '../../../../components/Carousel';
 
 const Banner = () => {
   return (
-    <div className="bg-rose-500" style={{ height: 'calc(100vh - 120px)' }}>
+    <div
+      className="bg-rose-500 relative"
+      style={{ height: 'calc(100vh - 120px)' }}
+    >
       <Carousel />
+      <div className="absolute top-1/4 lg:left-0 bg-cyan-500/40 mt-6 ml-2 mr-2 md:mb-0 md:mr-0 md:ml-6 lg:ml-12 p-6 roudned-3xl">
+        <div className="text-white font-bold">
+          <h1 className="text-2xl lg:text-5xl">
+            Travel and Share Your Experience
+          </h1>
+          <p className="text-lg lg:text-3xl text-left md:text-center pt-2 md:pt-3">
+            Be a happy Person
+          </p>
+        </div>
+        <div className="w-fit md:mx-auto mt-3 md:mt-6">
+          <button className="btn">Explore</button>
+        </div>
+      </div>
     </div>
   );
 };
